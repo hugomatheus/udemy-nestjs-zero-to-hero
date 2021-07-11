6bb0f336e573593a1909fb2d5d128ea65766653a
Hugo <hugo.azevedo@al.senai.br>
2021-07-11 15:43:08 -0300

    typeorm cli and migrations
    add typeorm cli 
    create migration tasks

---
+++ src/database/migrations/1626027964577-CreateTasks.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateTasks1626027964577 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'tasks',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+          },
+          {
+            name: 'title',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'status',
+            type: 'enum',
+            enum: ['OPEN', 'IN_PROGRESS', 'DONE'],
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+      true,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('tasks');
+  }
+}
