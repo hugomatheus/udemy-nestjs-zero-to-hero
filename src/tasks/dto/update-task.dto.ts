addd8956d8cf169809470a8da78114fdd409661b
Hugo <hugo.azevedo@al.senai.br>
2021-07-18 07:39:22 -0300

    remove import

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,5 @@
-import { PartialType } from '@nestjs/mapped-types';
 import { IsEnum, IsNotEmpty } from 'class-validator';
 import { TaskStatus } from '../task-status.enum';
-import { CreateTaskDto } from './create-task.dto';
 
 export class UpdateTaskDto {
   @IsNotEmpty()
